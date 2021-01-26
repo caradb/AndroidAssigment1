f1b55323dfe1d1317e20476cadcf4e3ecc5cecad
caradb <caraberube93@gmail.com>
2021-01-26 11:22:03 -0400

    last update

--- Assignment1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ Assignment1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toastButton: Button = findViewById(R.id.toast_button)
+        toastButton.setOnClickListener { makeToast() }
+
+        val countUpButton: Button = findViewById(R.id.plus_button)
+        countUpButton.setOnClickListener { countUp() }
+
+        val countDownButton: Button = findViewById(R.id.minus_button)
+        countDownButton.setOnClickListener { countDown() }
+    }
+
+    private fun makeToast(){
+        val counted: TextView = findViewById(R.id.countedTo)
+        val currentCount = counted.toString()
+        Toast.makeText(this, currentCount, Toast.LENGTH_SHORT).show()
+    }
+
+    private  fun countUp(){
+        val counted: TextView = findViewById(R.id.countedTo)
+        val countedUp = counted.toString() + 1
+        counted.text = countedUp
+    }
+
+    private  fun countDown(){
+        val counted: TextView = findViewById(R.id.countedTo)
+        val countedDown = counted.toString()
+        counted.text = countedDown
     }
 }
